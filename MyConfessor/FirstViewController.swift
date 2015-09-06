387b1b5571d5ea6604360c8207ecd71758e0dab5
Laura Paxton <laurapaxton3@gmail.com>
2015-09-05 21:00:55 -0700

    Initial Commit

---
+++ MyConfessor/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyConfessor
+//
+//  Created by My Mac on 9/5/15.
+//  Copyright (c) 2015 Carmel Heart Media. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
