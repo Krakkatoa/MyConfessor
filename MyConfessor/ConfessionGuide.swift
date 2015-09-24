12d77a451b70e1c238571340d40e4e66f9024586
Laura Paxton <laurapaxton3@gmail.com>
2015-09-23 19:33:25 -0700

    Problems with Update to Xcode 9
    I’ve been trying to fix problems that did not exist until this upgrade.
    The Q&A VC now crashes everything overtime. No idea why,

--- MyConfessor/ConfessionGuide.swift
+++ MyConfessor/ConfessionGuide.swift
@@ -16,9 +16,9 @@ struct ConfessionGuide {
     
     
     func randomFact() -> String {
-        var unsignedArrayCount = UInt32(factsArray.count)
-        var unsignedRandomNumber = arc4random_uniform(unsignedArrayCount)
-        var randomNumber = Int(unsignedRandomNumber)
+        let unsignedArrayCount = UInt32(factsArray.count)
+        let unsignedRandomNumber = arc4random_uniform(unsignedArrayCount)
+        let randomNumber = Int(unsignedRandomNumber)
         
         return factsArray[randomNumber]
     }
