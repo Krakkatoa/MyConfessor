756bda704a1b86b646c0618d821cac9256ec2af2
Laura Paxton <laurapaxton3@gmail.com>
2015-09-24 19:12:42 -0700

    Integrating Parse
    Has an error. Have not solved the problem yet.

--- MyConfessor/AppDelegate.swift
+++ MyConfessor/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse    
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,10 +18,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Parse Installation
+        Parse.setApplicationId("HTYnotuaSnkT4xrZxslB497gb7Q5JGMbpOBnJ0To", clientKey: "56xM6nJjCYMArgN4c0gsywG7J9OzOnobilwBpru5")
+        let notificationType: UIUserNotificationType = [UIUserNotificationType.Alert, UIUserNotificationType.Badge, UIUserNotificationType.Sound]
+        
+        let settings: UIUserNotificationSettings = UIUserNotificationSettings(forTypes: notificationType, categories: nil)
+        UIApplication.sharedApplication().registerUserNotificationSettings(settings)
+        UIApplication.sharedApplication().registerForRemoteNotifications()
+        
+        //This creates a identifier for each device, so we know which user is who
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(UIDevice.currentDevice().identifierForVendor!.UUIDString, forKey: "status")
+        
         return true
     }
-
-    func applicationWillResignActive(application: UIApplication) {
+    
+          func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
--- MyConfessor/SecondViewController.swift
+++ MyConfessor/SecondViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+    
+
 
 class SecondViewController: UIViewController {
 
