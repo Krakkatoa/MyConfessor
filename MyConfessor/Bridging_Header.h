6fef36b5b758a47b6d01d258c491e9bf9503c18c
Laura Paxton <laurapaxton3@gmail.com>
2015-09-25 21:05:12 -0700

    Adding Bridging-Header
    with a problem

---
+++ MyConfessor/Bridging_Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging_Header.h
+//  MyConfessor
+//
+//  Created by My Mac on 9/25/15.
+//  Copyright © 2015 Carmel Heart Media. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+
+#endif /* Bridging_Header_h */
