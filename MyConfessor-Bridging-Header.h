245a2106467f4464022ac3ca3dde517c3f84e380
Laura Paxton <laurapaxton3@gmail.com>
2015-09-25 21:28:30 -0700

    Product files disappearing
    need to fix

---
+++ MyConfessor-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  MyConfessor-Bridging-Header.h
+//  MyConfessor
+//
+//  Created by My Mac on 9/25/15.
+//  Copyright © 2015 Carmel Heart Media. All rights reserved.
+//
+
+#ifndef MyConfessor_Bridging_Header_h
+#define MyConfessor_Bridging_Header_h
+
+
+#endif /* MyConfessor_Bridging_Header_h */
+
+#import "Parse/Parse.h"
+
+
