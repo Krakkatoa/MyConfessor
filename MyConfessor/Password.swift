921656739e1c891e280d75b4b04f0c80361bee0c
Laura Paxton <laurapaxton3@gmail.com>
2015-09-28 16:03:50 -0700

    Beginning to add working elements
    to status and password VC’s. Parse is still not corrected, however.

---
+++ MyConfessor/Password.swift
@@ -0,0 +1,29 @@
+//
+//  Password.swift
+//  MyConfessor
+//
+//  Created by My Mac on 9/28/15.
+//  Copyright © 2015 Carmel Heart Media. All rights reserved.
+//
+
+import Foundation
+
+import Parse
+
+class PasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
